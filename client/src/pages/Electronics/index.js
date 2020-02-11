e5912c2db13621677450f496d5f1e0955436e9b9
jhowell89 <53121363+jhowell89@users.noreply.github.com>
2020-02-11 17:10:43 -0500

    buttons

--- client/src/pages/Electronics/index.js
+++ client/src/pages/Electronics/index.js
@@ -7,11 +7,13 @@ import React, { Component } from "react";
 import API from "../../utils/API.js";
 import "./style.css";
 
-class Electronics extends Component {
+class products extends Component {
     state = {
         products:[],
         search: "",
-        results: []
+        results: [],
+        cart:[]
+        
     };
     componentDidMount() {
         this.loadProduct();
@@ -45,7 +47,37 @@ class Electronics extends Component {
 
         // })
         // console.log(this.state.results)
-    }
+
+     };
+     getItem = (discription) => {
+         const products = this.state.products.find(item => item.discription === discription);
+         return products;
+     };
+     handleDetail = (discription) => {
+         const products = this.getItem(products);
+         this.setState(()=>{
+             return {detailproducts:products}
+
+         })
+     };
+     handleAddToCart = (product)=>{
+         this.props.addToCart(product);
+     }
+    //  addToCart = () => {
+    //      let tempProducts = [...this.state.products];
+    //      const index = tempProducts.indexOf(this.getItem(products));
+    //      const products = tempProducts[index];
+    //      products.inCart = true;
+    //      products.count = 1;
+    //      const price = products.price;
+    //      products.total = price;
+    //      this.setState(() => {
+    //          return { products: tempProducts, cart: [...this.state.cart,
+    //         products ]};
+    //      })
+    //      console.log("added to cart");
+    //  };
+     
 
     render() {
         console.log(this.state.friends)
@@ -56,6 +88,7 @@ class Electronics extends Component {
                 <SearchBar
                     handleFormSubmit={this.handleFormSubmit}
                     handleInputChange={this.handleInputChange}
+                    
                 />
                 <hr></hr>
                 <Wrapper>
@@ -67,16 +100,26 @@ class Electronics extends Component {
                             rating={product.rating}
                             price={product.price}
                             image={product.image}
+                            handleAddToCart={this.handleAddToCart}
+                            handleDetail={this.handleDetail}
+                            
                         />
-
-
+                        
+                    
+                        
+                    
                     )
                     )}
+                    
+                    
+                   
 
                 </Wrapper>
             </Container>
+            
 
         )
     }
 }
-export default Electronics;
+
+export default products;
