22d35328a946f5751dba5d7073aee8967b93b7a4
Ciprien Bossut <chefchino@live.com>
2020-02-15 11:31:21 -0500

    reactstrap working

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,33 +1,72 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
 
-function NavTabs() {
+const Navtabs = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/electronics"
-          className={window.location.pathname === "/electronics" ? "nav-link active" : "nav-link"}
-        >
-          Electronics
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/cart"
-          className={window.location.pathname === "/cart" ? "nav-link active" : "nav-link"}
-        >
-          Cart
-        </Link>
-      </li>
-
-    </ul>
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Home</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/electronics">Electronics</NavLink>
+            </NavItem>
+          </Nav>
+              <NavLink href="/cart">Cart</NavLink>
+        </Collapse>
+      </Navbar>
+    </div>
   );
 }
 
-export default NavTabs;
+export default Navtabs;
+
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+
+
+// function NavTabs() {
+//   return (
+//     <ul className="nav nav-tabs">
+//       <li className="nav-item">
+//         <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+//           Home
+//         </Link>
+//       </li>
+//       <li className="nav-item">
+//         <Link
+//           to="/electronics"
+//           className={window.location.pathname === "/electronics" ? "nav-link active" : "nav-link"}
+//         >
+//           Electronics
+//         </Link>
+//       </li>
+//       <li className="nav-item">
+//         <Link
+//           to="/cart"
+//           className={window.location.pathname === "/cart" ? "nav-link active" : "nav-link"}
+//         >
+//           Cart
+//         </Link>
+//       </li>
+
+//     </ul>
+//   );
+// }
+
+// export default NavTabs;
